49b868d5d0bd5e7bd2d21d5ce6be77d539c41b03
Nihal-a <nihalavulan1@gamil.com>
2021-10-23 11:17:16 +0530

    Routing structure changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import { useState,useEffect } from "react";
-import Landing from "./pages/Landing";
 import "./App.css";
-import { BrowserRouter as Router, Route ,Redirect,Switch} from "react-router-dom";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
+import { BrowserRouter as Router} from "react-router-dom";
+import Routed from "./Routed";
 
 function App() {
 
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
 
   return (
     <Router>
-      <Switch>
-      <Route exact path="/" component={Landing} />
-     <Route path="/signup">
-        {user ? <Redirect to="/" /> : <Signup />}
-      </Route> 
-      <Route path="/signin">
-        {user ? <Redirect to="/" /> : <Signin />}
-      </Route>
-      </Switch>
+      <Routed/>
     </Router>
   );
 }
---
+++ src/Routed.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import Landing from "./pages/Landing";
+import "./App.css";
+import {Route, Redirect, Switch,useHistory,useLocation} from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import { useDispatch } from "react-redux";
+import jwtDecode from "jwt-decode";
+import { LOGOUT } from "./constants/actionTypes";
+
+function Routed() {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const history = useHistory()
+  const dispatch = useDispatch()
+  const location = useLocation()
+
+  useEffect(() => {
+    const token  = user?.token;
+
+    if(token){
+        const decodedToken = jwtDecode(token)
+        if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location])
+
+  const logout = (e) => {
+    e.preventDefault()
+    dispatch({type : LOGOUT});
+    
+    history.push('/');
+    
+    setUser(null);
+  }
+
+    return (
+        <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/signup">
+                {user ? <Redirect to="/" /> : <Signup />}
+            </Route>
+            <Route path="/signin">
+                {user ? <Redirect to="/" /> : <Signin />}
+            </Route>
+        </Switch>
+    )
+}
+
+export default Routed

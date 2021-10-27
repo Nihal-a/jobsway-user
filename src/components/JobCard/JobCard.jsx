544b1bd1c8589fa9676ad4fdb403a268246eb9eb
Nihal-a <nihalavulan1@gamil.com>
2021-10-27 13:02:30 +0530

    jobs Card companent completed

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -5,9 +5,9 @@ import { Icon } from '@iconify/react';
 
 function JobCard() {
     return (
-        <Link className="w-full h-52 rounded-md p-4" style={{ backgroundColor: '#2c2c2c' }}>
+        <div className="w-full h-62 rounded-md p-4" style={{ backgroundColor: '#2c2c2c' }}>
             <div className="flex justify-between">
-                <div className="flex items-center">
+                <Link className="flex items-center">
                     <img src={googleLogo} alt="company logo" className="w-14 h-14 rounded-md" />
                     <div className="ml-3">
                     <h6 className=" text-2xl font-semibold text-white">Google</h6>
@@ -15,10 +15,24 @@ function JobCard() {
                         <Icon icon="akar-icons:location" className="text-dark"/><p className="text-dark font-light ml-1">Bengluru,India</p>
                     </div>
                     </div>
-                </div>
+                </Link>
                 <div className="text-white">10 days ago</div>
             </div>
-        </Link>
+            <div className="flex flex-col">
+                <h2 className="text-3xl font-semibold text-white mt-3">Sr.Web Devoloper</h2>
+                <div className="flex items-center">
+                    <span className="flex items-center mt-2">
+                        <Icon icon="bx:bx-rupee" height="34" color="#00B512" />
+                        <h6 className="text-2xl font-semibold" style={{color:'#00B512'}}>30000 - 50000</h6>
+                    </span>
+                    <h5 className="text-white font-light mt-2 ml-5">Full time</h5>
+                </div>
+            </div>
+            <div className="flex justify-between mt-2 ml-2">
+                <p className="text-white">4 - 8 year of Experience</p>
+                <Link className="bg-white text-primary py-2 px-4 rounded-md font-semibold">Know More</Link>
+            </div>
+        </div>
     )
 }
 
--- src/components/featuredJobs/FeaturedJobs.jsx
+++ src/components/featuredJobs/FeaturedJobs.jsx
@@ -4,10 +4,16 @@ import JobCard from '../JobCard/JobCard';
 function FeaturedJobs() {
     const num = 0;
     return (
-        <div className=" h-screen w-screen">
+        <div className="h-screen w-screen ">
             <h4 className="text-4xl font-semibold text-center py-8"><span className="text-primary">Featured</span> Jobs.</h4>
-            <div className="container mx-auto">
+            <div className="container mx-auto pb-8">
                 <div className="grid grid-cols-2 gap-4">
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
+                    <JobCard />
                     <JobCard />
                     <JobCard />
                 </div>

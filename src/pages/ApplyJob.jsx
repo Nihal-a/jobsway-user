afc860674653cbdbc07595eb772e13969df76e13
Nihal-a <nihalavulan1@gamil.com>
2021-12-02 21:28:21 +0530

    Evn updated and apply job function started

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -1,44 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/navbar/Navbar'
 import noImage from "../assets/images/noImage.jpg";
 import { Link } from 'react-router-dom';
 
+const initialState = {firstName:'',email : '' , location : '' , lastName : '' , phone : '' , experience : '' , portfolio : ''}
 
 const ApplyJob = () => {
 
-    const handleImageChange = () => {
+    const [formData, setFormData] = useState(initialState)
+  const [image, setImage] = useState(noImage);
 
+
+    const handleImageChange = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.readyState === 2) {
+            setImage(reader.result);
+          }
+        };
+        reader.readAsDataURL(e.target.files[0]);
+      };
+
+    const handleChange = (e) => {
+        e.preventDefault()
+        setFormData({...formData,[e.target.name] : e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData);
     }
 
     return (
         <div>
             <Navbar />
 
-            <form className="container mx-auto mt-40 ">
+            <form className="container mx-auto mt-40 " onSubmit={handleSubmit}>
                 <h2 className="text-3xl font-semibold">Applying for <span className="text-primary">Sr. Web Developer</span></h2>
                 <div className="mt-3 flex">
                     <div className="flex flex-col w-1/3 p-2 mt-3">
-                        <input type="text" name="" id="" placeholder="First Name" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
-                        <input type="text" name="" id="" placeholder="Email" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
-                        <input type="text" name="" id="" placeholder="Location" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="firstName" id="" placeholder="First Name" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="email" id="" placeholder="Email" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="location" id="" placeholder="Location" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
                         <div className="">
                             <label className="ml-3 mt-2 m-1" htmlFor="upload">Upload Resume : </label>
-                        <input type="file" name="upload" id="" className="bg-secondary p-4 ml-2 rounded-md" />
+                        <input required type="file" name="upload" id="" className="bg-secondary p-4 ml-2 rounded-md" />
                         <p className="text-xs m-3">Don’t have one? No worries <Link className="text-primary underline"> Create a JobsWay Resume</Link></p>
                         </div>
                     </div>
                     <div className="flex flex-col w-1/3 p-2 mt-3">
-                        <input type="text" name="" id="" placeholder="Last Name" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
-                        <input type="text" name="" id="" placeholder="Phone" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
-                        <input type="text" name="" id="" placeholder="Years of Exp." className="bg-secondary p-4 m-2 mt-5 rounded-md" />
-                        <input type="text" name="" id="" placeholder="First Name" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="lastName" id="" placeholder="Last Name" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="phone" id="" placeholder="Phone" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="number" name="experience" id="" placeholder="Years of Exp." className="bg-secondary p-4 m-2 mt-5 rounded-md" />
+                        <input onChange={handleChange} type="text" name="portfolio" id="" placeholder="Portfolio link" className="bg-secondary p-4 m-2 mt-5 rounded-md" />
                     </div>
                     <div className="w-1/3">
                     <div className="w-full h-40 mt-4 flex items-center flex-col mt-24">
-                    <img src={noImage} alt="no image" className="w-56 rounded-md" />
+                    <img src={image} alt="no image" className="w-56 h-56 rounded-md" />
                         <div className="bg-black relative overflow-hidden h-14 mt-3 flex items-center p-4 rounded-md cursor-pointer">
                         <span className="text-sm text-white font-light cursor-pointer">
-                           Upload Logo
+                           Upload Photo
                          </span>
                      <input
                             type="file"

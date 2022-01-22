0aef1674c0a31f883e4a2c587ed9b0ef3fadfb02
Nihal-a <nihalavulan1@gamil.com>
2022-01-22 10:55:11 +0530

    interest page designed

---
+++ src/components/ResumeComponenets/ExtraDetails.jsx
@@ -0,0 +1,75 @@
+import { Button } from '@material-tailwind/react';
+import React from 'react';
+
+const ExtraDetails = ({prevStep , nextStep}) => {
+  return <div className='w-full'>
+  <h3 className='text-xl text-center'>Extra Information</h3>
+<div className="bg-secondary p-10 rounded-md w-full mt-3">
+<div className="flex flex-col">
+    <div className="">
+        <h4 className="m-2">Skills : </h4>
+<div className="w-full flex">
+<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Skill 1" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Skill 2" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Skill 3" />
+</div>
+</div>
+<div className="w-full flex">
+<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Skill 4" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Skill 5" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Skill 6" />
+</div>
+</div>
+</div>
+<hr className='my-4'/>
+    <div className="">
+        <h4 className="m-2">Languages : </h4>
+<div className="w-full flex">
+<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Language 1" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Language 2" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Language 3" />
+</div>
+</div>
+</div>
+<hr className='my-2'/>
+    <div className="">
+        <h4 className="m-2">Interests : </h4>
+<div className="w-full flex">
+<div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Interest 1" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Interest 2" />
+</div>
+<div className="w-full md:w-1/2 px-3">
+   <input className="appearance-none block w-full bg-grey-200 text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="number" placeholder="Interest 3" />
+</div>
+</div>
+</div>
+<hr />
+    
+<div className="flex items-center justify-center font-light">
+<Button color="gray" className="m-2" onClick={prevStep} >Prev</Button>
+<Button className="m-2" onClick={nextStep}  >Next</Button>
+</div>
+</div>
+</div>
+</div>
+};
+
+export default ExtraDetails;
--- src/pages/CreateResume.jsx
+++ src/pages/CreateResume.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Education from '../components/ResumeComponenets/Education';
 import Experience from '../components/ResumeComponenets/Experience';
+import ExtraDetails from '../components/ResumeComponenets/ExtraDetails';
 import Indroduction from '../components/ResumeComponenets/Indroduction';
 import Profile from '../components/ResumeComponenets/Profile';
 import ProjectDetails from '../components/ResumeComponenets/ProjectDetails';
@@ -93,6 +94,8 @@ const CreateResume = () => {
           return <ProjectDetails nextStep={nextStep} prevStep={prevStep}/>
           case 4:
           return <Education nextStep={nextStep} prevStep={prevStep}/>
+          case 5:
+          return <ExtraDetails nextStep={nextStep} prevStep={prevStep}/>
       default:
         break;
     }
@@ -118,60 +121,3 @@ const CreateResume = () => {
 
 export default CreateResume;
 
-
-
-// <form className="w-full max-w-4xl flex flex-col">
-// <div className="flex flex-wrap -mx-3 mb-6">
-//   <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-first-name">
-//       First Name
-//     </label>
-//     <input className="appearance-none block w-full bg-secondary text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="" />
-//     {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
-//   </div>
-//   <div className="w-full md:w-1/2 px-3">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-last-name">
-//       Last Name
-//     </label>
-//     <input className="appearance-none block w-full bg-secondary text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="" />
-//   </div>
-// </div>
-// <div className="flex flex-wrap -mx-3 mb-6">
-//   <div className="w-full px-3">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-password">
-//       Job title
-//     </label>
-//     <input className="appearance-none block w-full bg-secondary text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="" />
-//     {/* <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p> */}
-//   </div>
-// </div>
-// <div className="flex flex-wrap -mx-3 mb-2">
-//   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-city">
-//       City
-//     </label>
-//     <input className="appearance-none block w-full bg-secondary text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-//   </div>
-//   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-state">
-//       State
-//     </label>
-//     <div className="relative">
-//       <select className="block appearance-none w-full bg-secondary border  text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-//         <option>New Mexico</option>
-//         <option>Missouri</option>
-//         <option>Texas</option>
-//       </select>
-//       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-//         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-//       </div>
-//     </div>
-//   </div>
-//   <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-//     <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2" htmlFor="grid-zip">
-//       Zip
-//     </label>
-//     <input className="appearance-none block w-full bg-secondary text-gray-700 border  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder={90210} />
-//   </div>
-// </div>
-// </form>

8bdbbe0979f976a91673b38412dc532538c23b6c
Nihal-a <nihalavulan1@gamil.com>
2022-01-30 00:57:08 +0530

    landing page responsive ready

--- src/components/postJob/PostJob.jsx
+++ src/components/postJob/PostJob.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 
 function PostJob() {
     return (
-        <div className="container w-full mx-auto h-46 bg-primary rounded-md p-8 mb-8 flex items-center justify-between">
+       <div className="p-3">
+            <div className="container w-full mx-auto h-46 bg-primary rounded-md p-8 mb-8 flex items-center justify-between flex-col lg:flex-row gap-4">
             <div className="">
-                <h4 className="text-3xl font-semibold text-white">Does your company needs great employees?</h4>
-                <p className="text-lg text-white mt-5 ">Post the job and hire the right one for your company.</p>
+                <h4 className="text-2xl font-semibold text-white md:text-3xl">Does your company needs great employees?</h4>
+                <p className="text-md md:text-lg text-white mt-5 ">Post the job and hire the right one for your company.</p>
             </div>
             <a href="http://localhost:3002/" className="text-primary bg-white py-3 px-20 rounded-md text-lg font-semibold">Post a Job</a>
         </div>
+       </div>
     )
 }
 

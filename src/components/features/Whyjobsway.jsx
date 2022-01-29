aa518766721649bb6b415513559e41792d13ff60
Nihal-a <nihalavulan1@gamil.com>
2022-01-30 00:38:47 +0530

    Why jobsWay section resposive

--- src/components/features/Whyjobsway.jsx
+++ src/components/features/Whyjobsway.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function Whyjobsway() {
     return (
         <div className="mx-auto flex flex-col items-center py-10 bg-secondary">
-            <h4 className="text-4xl font-semibold text-center ">Why <span className="text-primary">JobsWay.</span></h4>
-            <h3 className="font-semibold text-dark text-4xl max-w-2xl text-center mt-8">Make the right hires, faster with
+            <h4 className="text-2xl md:text-4xl font-bold text-center ">Why <span className="text-primary">JobsWay?</span></h4>
+            <h3 className="font-semibold text-dark text-xl md:text-3xl max-w-sm md:max-w-2xl text-center mt-8">Make the right hires, faster with
                 world’s leading recruiting platform</h3>
-            <p className="max-w-6xl text-center text-lg mt-8 text-dark">In this competitive era, the education among the people is so increasing that the jobs for them are now decreasing. The companies even want the people who are best in their fields. At that time, it becomes difficult to find people who are intelligent enough to be hired. The work for the companies also increases to find the people who can fulfill their requirements. JobsWay connect the best companies to the skilled people.</p>
+            <p className="max-w-3xl text-center text-md mt-8 text-dark p-3 md:max-w-6xl md:text-lg">In this competitive era, the education among the people is so increasing that the jobs for them are now decreasing. The companies even want the people who are best in their fields. At that time, it becomes difficult to find people who are intelligent enough to be hired. The work for the companies also increases to find the people who can fulfill their requirements. JobsWay connect the best companies to the skilled people.</p>
         </div>
     )
 }

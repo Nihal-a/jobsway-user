6fabccbff16c3286f099bb1a525cab40dc41d8c3
Nihal-a <nihalavulan1@gamil.com>
2022-02-02 13:19:45 +0530

    post job link changed

--- src/components/postJob/PostJob.jsx
+++ src/components/postJob/PostJob.jsx
@@ -9,7 +9,7 @@ function PostJob() {
                 <h4 className="text-2xl font-semibold text-white md:text-3xl">Does your company needs great employees?</h4>
                 <p className="text-md md:text-lg text-white mt-5 ">Post the job and hire the right one for your company.</p>
             </div>
-            <a href="http://localhost:3002/" className="text-primary bg-white py-3 px-20 rounded-md text-lg font-semibold">Post a Job</a>
+            <a href="https://company.jobsway.online/" target={'_blank'} className="text-primary bg-white py-3 px-20 rounded-md text-lg font-semibold">Post a Job</a>
         </div>
        </div>
     )

09e6e8c5fa0ed4489f23292bb43b579ab6403e00
Nihal-a <nihalavulan1@gamil.com>
2022-02-02 14:03:30 +0530

    deployment link changed

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const API = axios.create({ baseURL:process.env.REACT_APP_PRODUCTION_BASE_URL })
 // const API = axios.create({ baseURL:process.env.REACT_APP_DEV_BASE_URL})
-const API = axios.create({ baseURL : "http://www.userapi.jobsway.online/api/v1/user"})
+const API = axios.create({ baseURL : "https://www.userapi.jobsway.online/api/v1/user"})
 
 
 //Auth

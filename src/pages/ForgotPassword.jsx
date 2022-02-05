ec70fc39d108635df2f51c3d80638fba1dcaf333
Nihal-a <nihalavulan1@gamil.com>
2022-02-05 11:05:34 +0530

    Forgot password validation done

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -7,6 +7,8 @@ import { forgotPassword } from "../actions/auth";
 import Footer from "../components/footer/Footer";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
 import Navbar from "../components/navbar/Navbar";
+import { useForm } from "react-hook-form";
+
 
 const initialState = { phone : '' }
 
@@ -15,13 +17,13 @@ const ForgotPassword = () => {
     const dispatch = useDispatch()
     const history = useHistory()
     const [loading, setLoading] = useState(false)
+    const { register, formState: { errors }, handleSubmit: validateSubmit } = useForm();
 
   const handleChange = (e) => {
     e.preventDefault()
     setFormData({...formData,[e.target.name] : e.target.value})
   };
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = () => {
     setLoading(true)
     dispatch(forgotPassword(formData , history , setLoading))
   };
@@ -39,16 +41,20 @@ const ForgotPassword = () => {
                 <h3 className="text-3xl font-semibold mt-4">Reset Password.</h3>
                 <p className="text-center mt-3">Verify your phone number with OTP for setting the new Password</p>
             </div>
+            <form onSubmit={validateSubmit(handleSubmit)} action="" className="flex flex-col items-center w-3/4 text-left">
             <input
+              {...register("phone", { required:  true  , minLength:10 , maxLength:10 , pattern : { value : /^[0-9]{10}$/ }})}
               onChange={handleChange}
               type="tel"
               name="phone"
               id=""
               placeholder="Phone Number"
               className="bg-secondary p-4 m-2 mt-5 rounded-md w-3/4 max-w-sm"
-              required
-            />
-            <button className="py-2 px-7 rounded-md text-white mt-3 bg-primary" onClick={handleSubmit}>Get OTP</button>
+              />
+              {errors.phone  && <p className="text-danger text-xs text-left m-1">Enter a Valid Phone Number</p>}
+
+            <button className="py-2 px-7 rounded-md text-white mt-3 bg-primary" type="submit">Get OTP</button>
+            </form>
       </div>
       <div className="absolute bottom-0">
          <Footer/>

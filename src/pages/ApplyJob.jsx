947da0a97d0af20397f58eeac3fd87a27f3271b4
Nihal-a <nihalavulan1@gamil.com>
2022-02-06 09:25:40 +0530

    Apply job issue fixed

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/navbar/Navbar";
-import avatar from "../assets/images/avatar_image.png";
+import noImage from "../assets/images/noImage.jpg";
 import { Link, useLocation, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner/LoadingSpinner";
@@ -12,9 +12,6 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { APPLYJOBS } from "../constants/actionTypes";
 import { useMediaQuery } from "react-responsive";
-import { useForm } from "react-hook-form";
-
-
 
 const initialState = {
   firstName: "",
@@ -36,8 +33,6 @@ const ApplyJob = () => {
   const [pdf, setPdf] = useState("");
   const [viewpdf, setViewPdf] = useState("");
   const isMobile = useMediaQuery({ query: `(max-width: 640px)` });
-  const { register, formState: { errors }, handleSubmit: validateSubmit } = useForm();
-
 
   const dispatch = useDispatch();
 
@@ -54,11 +49,9 @@ const ApplyJob = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = () => {
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
     setLoading(true);
-    
     var fileData = new FormData();
 
     fileData.append("firstName", formData.firstName || name[0]);
@@ -95,6 +88,7 @@ const ApplyJob = () => {
 
   const handlePdfChange = (e) => {
     e.preventDefault();
+
     setPdf(e.target.files[0]);
 
     const fileReader = new FileReader();
@@ -117,7 +111,7 @@ const ApplyJob = () => {
       <form
         action=""
         className="container mx-auto mt-40"
-        onSubmit={validateSubmit(handleSubmit)}
+        onSubmit={handleSubmit}
       >
         <h2 className={`${isMobile ? 'text-lg' :  'text-3xl'} font-semibold ${isMobile && 'text-center'}`}>
           Applying for{" "}
@@ -137,7 +131,7 @@ const ApplyJob = () => {
           {/* Photo upload area */}
           <div className="pb-10">
             <div className="w-full h-40 mt-4 flex items-center flex-col mt-24">
-              <img src={image ? image : avatar} alt="no image" className="h-40 rounded-md" />
+              <img src={image ? image : noImage} alt="no image" className="h-40"/>
 
               <ImageInput setImage={setImage} />
             </div>
@@ -145,94 +139,67 @@ const ApplyJob = () => {
         </div>
         <div className="grid grid-cols-2 ">
           <div className="col-span-2 p-5">
-            <div className={`w-full flex items-center justify-between gap-2 ${isMobile && 'flex-col '}`}>
-              <div className="w-full flex-1">
+            <div className={`w-full flex items-center justify-between ${isMobile && 'flex-col '}`}>
               <input
                 onChange={handleChange}
                 type="text"
                 name="firstName"
                 id=""
                 placeholder="First Name"
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
                 value={name[0]}
-                {...register("firstName", { required: true })}
-                />
-                  {errors.firstName  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-              </div>
-              <div className="w-full flex-1">
+              />
               <input
                 onChange={handleChange}
                 type="text"
                 name="lastName"
                 id=""
                 placeholder="Last Name"
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
                 value={name[1]}
-                {...register("lastName", { required: true })}
               />
-                {errors.lastName  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-              </div>
             </div>
-            <div className={`w-full flex  items-center justify-between gap-2 ${isMobile && 'flex-col '}`}>
-              <div className="w-full flex-1">
+            <div className={`w-full flex items-center justify-between ${isMobile && 'flex-col '}`}>
               <input
-                {...register("email", { required : true   , pattern : { value :  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i } })}
                 onChange={handleChange}
                 type="email"
                 name="email"
                 id=""
                 placeholder="Email"
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
                 value={user?.user.authmode == 'email' ? user?.user.email : formData.email}
               />
-                {errors.email  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-              
-              </div>
-              <div className="w-full flex-1">
+
               <input
                 onChange={handleChange}
                 type="tel"
                 name="phone"
                 id=""
                 placeholder="Phone"
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
                 value={user?.user.authmode == 'phone' ? user?.user.phone : formData.phone}
-                {...register("phone", { required:  user?.user.authmode == 'phone' ? true :false  , minLength:10 , maxLength:10})}
               />
-                {errors.phone  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-              </div>
             </div>
 
-            <div className={`w-full flex items-center justify-between gap-2 ${isMobile && 'flex-col '}`}>
-              <div className="w-full flex-1">
+            <div className={`w-full flex items-center justify-between ${isMobile && 'flex-col '}`}>
               <input
-                  onChange={handleChange}
-                  type="text"
-                  name="location"
-                  id=""
-                  placeholder="Location"
-                  className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
-                {...register("location", { required: true })}
-                />
-                {errors.location  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
+                onChange={handleChange}
+                type="text"
+                name="location"
+                id=""
+                placeholder="Location"
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+              />
 
-              </div>
-
-             <div className="w-full flex-1">
-             <input
+              <input
                 onChange={handleChange}
                 type="number"
                 name="experience"
                 id=""
                 placeholder="Years of Exp."
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
                 min={0}
-                {...register("experience", { required: true })}
-
               />
-                {errors.experience  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-
-             </div>
             </div>
 
             <div className={`w-full flex items-center justify-between ${isMobile && 'flex-col '}`}>
@@ -247,10 +214,7 @@ const ApplyJob = () => {
                   className={`bg-secondary p-4 rounded-md  ${isMobile && 'w-full'}`}
                   accept="application/pdf,application/vnd.ms-excel"
                   onChange={handlePdfChange}
-                {...register("upload", { required: true  })}
                 />
-                {errors.upload  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-
                 {pdf && (
                   <button className="underline mt-1" onClick={handleViewPdf}>
                     View Pdf
@@ -266,18 +230,14 @@ const ApplyJob = () => {
 
                 </div>
 
-             <div className="flex-1 w-full">
-             <input
+              <input
                 onChange={handleChange}
                 type="text"
                 name="portfolio"
                 id=""
                 placeholder="Portfolio link"
-                className={`bg-secondary w-full p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
-                {...register("portfolio", { required: true  })}
+                className={`bg-secondary flex-1 p-4 m-2 mt-5 rounded-md ${isMobile && 'w-full'}`}
               />
-                {errors.portfolio  && <p className="text-danger text-xs text-left m-1">This field is required</p>}
-             </div>
             </div>
           </div>
         </div>
